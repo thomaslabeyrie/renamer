ad448fc718aa94aaa31ae3918adc3d094749a013
thomas.labeyrie7@gmail.com <thomas.labeyrie7@gmail.com>
2025-04-22 12:57:50 +0200

    created the renamer program

---
+++ README.md
@@ -0,0 +1,7 @@
+# Renamer
+
+I needed to rename multiple files on my system so as a beginner programmer I wrote a simple C program to format the name of a file.
+
+Spaces " " are replaced with underscores "\_".
+Uppercase letters "A" are replaced with lowercase ones "a".
+So "Test File.txt" is renamed to "test_file.txt"
---
+++ renamer.c
@@ -0,0 +1,61 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* format(char* name);
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage : ./renamer file\n");
+        return 1;
+    }
+
+    char* old_name = argv[1];
+    char* new_name = format(old_name);
+
+    if (rename(old_name, new_name) == 0)
+    {
+        printf("File renamed: %s\n", new_name);
+    }
+    else
+    { 
+        perror("Error");
+    }
+
+    free(new_name);
+}
+
+char* format(char* name)
+{
+    int length = strlen(name);
+    char* formatted = malloc(length + 1);
+
+    if (!formatted)
+    {
+        perror("malloc");
+        exit(1);
+    }
+    
+    for (int i = 0; i < length; i++)
+    {
+        if (isupper(name[i]))
+        {
+            formatted[i] = tolower(name[i]);
+        }
+        else if (name[i] == ' ')
+        {
+            formatted[i] = '_';
+        }
+        else
+        {
+            formatted[i] = name[i];
+        }
+    }
+
+    formatted[length] = '\0';
+    return formatted;
+}
