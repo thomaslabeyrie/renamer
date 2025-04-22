49d66161589750eb271130d3e79c82f463074319
thomas.labeyrie7@gmail.com <thomas.labeyrie7@gmail.com>
2025-04-22 19:43:42 +0200

    checked for similarity in old and new names before calling rename, made name a constant to explicitly state that we are not changing it, removed the conditional for uppercase

--- renamer.c
+++ renamer.c
@@ -1,39 +1,12 @@
 #include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char* format(char* name);
-
-int main(int argc, char* argv[])
-{
-    if (argc != 2)
-    {
-        printf("Usage : ./renamer file\n");
-        return 1;
-    }
-
-    char* old_name = argv[1];
-    char* new_name = format(old_name);
-
-    if (rename(old_name, new_name) == 0)
-    {
-        printf("File renamed: %s\n", new_name);
-    }
-    else if (strcmp(old_name, new_name) == 0)
-    {
-        printf("File name is already formatted.\n");
-    }
-    else
-    { 
-        perror("Error");
-    }
-
-    free(new_name);
-}
-
-char* format(char* name)
+// Downcases all letters and replaces space with underscores
+char* format(const char* name)
 {
     int length = strlen(name);
     char* formatted = malloc(length + 1);
@@ -46,20 +19,43 @@ char* format(char* name)
     
     for (int i = 0; i < length; i++)
     {
-        if (isupper(name[i]))
-        {
-            formatted[i] = tolower(name[i]);
-        }
-        else if (name[i] == ' ')
+        if (name[i] == ' ')
         {
             formatted[i] = '_';
         }
         else
         {
-            formatted[i] = name[i];
+            formatted[i] = tolower(name[i]);
         }
     }
 
     formatted[length] = '\0';
     return formatted;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage : ./renamer file\n");
+        return 1;
+    }
+
+    char* old_name = argv[1];
+    char* new_name = format(old_name);
+
+    if (strcmp(old_name, new_name) == 0)
+    {
+        printf("File name is already formatted.\n");
+    }
+    else if (rename(old_name, new_name) == 0)
+    {
+        printf("File renamed: %s\n", new_name);
+    }
+    else
+    { 
+        perror("Error");
+    }
+
+    free(new_name);
 }
